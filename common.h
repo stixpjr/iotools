0399f4898af682c6c36a76c834ec222c45624d5a
stix <stix>
2006-07-26 12:12:38 +0000

    autoconf updates.

--- common.h
+++ common.h
@@ -1,4 +1,4 @@
-/* $Id: common.h,v 1.2 2003/07/22 13:50:43 stix Exp stix $ */
+/* $Id: common.h,v 1.3 2003/07/23 14:51:39 stix Exp stix $ */
 
 /*
  * Copyright (c) 2003 Paul Ripke. All rights reserved.
@@ -31,8 +31,16 @@
  * possibility of such damage.
  */
 
-#ifndef _COMMON_H_
-#define _COMMON_H_
+#ifndef COMMON_H
+#define COMMON_H 1
+
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#if !HAVE_BZERO && HAVE_MEMSET
+#define bzero(p, n) ((void) memset((p), 0, (n)))
+#endif
 
 /*
  * Custom assert which won't be disabled with "NDEBUG".
@@ -40,7 +48,7 @@
 #define MYASSERT(expr, err)				\
 do {							\
 	if (!(expr)) {					\
-		aborted = 1;				\
+		flAborted = 1;				\
 		perror(err);				\
 		_exit(1);				\
 	}						\
@@ -62,4 +70,4 @@ int64_t	getnum(char *);
 void	initblock(char *, long, dataType, int64_t);
 void	*getshm(long size);
 
-#endif /* !_COMMON_H_ */
+#endif /* !COMMON_H */
