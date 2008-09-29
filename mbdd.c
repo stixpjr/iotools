5d003adbd8f89e2cbae177ee3175419d69e3c1fd
stix <stix>
2008-09-29 00:42:56 +0000

    Allow multiple destinations - like a high speed tee(1).

--- mbdd.c
+++ mbdd.c
@@ -1,4 +1,4 @@
-/* $Id: mbdd.c,v 1.5 2008/09/17 10:17:19 stix Exp $ */
+/* $Id: mbdd.c,v 1.6 2008/09/17 10:51:24 stix Exp $ */
 
 /*
  * Copyright (c) 2006 Paul Ripke. All rights reserved.
@@ -38,10 +38,11 @@
 #error "pthreads required!"
 #endif
 
-static char const rcsid[] = "$Id: mbdd.c,v 1.5 2008/09/17 10:17:19 stix Exp $";
+static char const rcsid[] = "$Id: mbdd.c,v 1.6 2008/09/17 10:51:24 stix Exp $";
 
 /* Prototypes */
 static void	*reader(void *);
+static void	*writer(void *);
 static void	*status(void *);
 static void	cleanup(int);
 static void	usage();
@@ -51,29 +52,32 @@ static char **buf;
 static long bufSize, partialReads;
 static int flAborted, flFinished, numBufs;
 static size_t remainder;
-static int64_t totalWritten;
+static int destCount;
+static char **destNames;
+static int64_t *totalWritten;
+static unsigned long *bufSum, *bufSamples;
+static int *outfds;
 
 static pthread_mutex_t lock;
 static pthread_cond_t less;
-static pthread_cond_t more;
-static int fullBufs;
+static pthread_cond_t *more;
+static int *fullBufs;
 
 int
 main(int argc, char **argv)
 {
-	int bufNum, c, i, outfd, flQuiet;
-	ssize_t numWrite, writeSize;
-	unsigned long bufSum, bufSamples;
+	int c, i, flQuiet, stdoutcopy;
 	float duration;
 	struct timeval tpstart, tpend;
-	pthread_t tid;
+	pthread_t reader_tid;
+	pthread_t *writer_tids;
 	pthread_attr_t attr;
 	pthread_t status_tid;
 	int flVerbose = 0;
 
 	/* close off stdout, so stdio doesn't play with it */
-	outfd = dup(STDOUT_FILENO);
-	if (outfd < 0) {
+	stdoutcopy = dup(STDOUT_FILENO);
+	if (stdoutcopy < 0) {
 		perror("dup on stdout failed");
 		exit(1);
 	}
@@ -83,8 +87,9 @@ main(int argc, char **argv)
 	bufSize = 64*1024;
 	numBufs = 16;
 	flQuiet = 0;
+	destCount = 1;
 
-	while ((c = getopt(argc, argv, "b:n:qv")) != EOF) {
+	while ((c = getopt(argc, argv, "b:n:qsv")) != EOF) {
 		switch (c) {
 		case 'b':
 			bufSize = getnum(optarg);
@@ -94,6 +99,10 @@ main(int argc, char **argv)
 			break;
 		case 'q':
 			flQuiet = 1;
+			break;
+		case 's':
+			destCount = 0;
+			close(stdoutcopy);
 			break;
 		case 'v':
 			flVerbose = 1;
@@ -106,6 +115,56 @@ main(int argc, char **argv)
 	}
 	if (numBufs < 2) {
 		fprintf(stderr, "Buffer count must be > 2\n");
+		exit(1);
+	}
+
+	/* Open additional destinations */
+
+	argv += optind;
+	argc -= optind;
+	if (destCount + argc <= 0) {
+		fprintf(stderr, "No destinations specified.\n");
+		exit(1);
+	}
+	outfds = malloc((sizeof *outfds) * (destCount + argc));
+	destNames = malloc((sizeof *destNames) * (destCount + argc));
+	if (outfds == NULL ||
+	    destNames == NULL) {
+		fprintf(stderr, "malloc failed.\n");
+		exit(1);
+	}
+
+	if (destCount == 1) {
+		outfds[0] = stdoutcopy;
+		destNames[0] = "stdout";
+	}
+	while (*argv) {
+		outfds[destCount] = open(*argv, O_WRONLY|O_CREAT, DEFFILEMODE);
+		if (outfds[destCount] < 0) {
+			fprintf(stderr, "Unable to open '%s' for write: %s\n", *argv,
+			    strerror(errno));
+			exit(1);
+		}
+		destNames[destCount] = *argv;
+		destCount++;
+		argv++;
+	}
+
+	/* Allocate all the arrays that are sized per destination */
+
+	totalWritten = malloc(sizeof(*totalWritten) * destCount);
+	bufSamples = malloc(sizeof(*bufSamples) * destCount);
+	bufSum = malloc(sizeof(*bufSum) * destCount);
+	fullBufs = malloc(sizeof(*fullBufs) * destCount);
+	writer_tids = malloc(sizeof(*writer_tids) * destCount);
+	more = malloc(sizeof(*more) * destCount);
+	if (totalWritten == NULL ||
+	    bufSamples == NULL ||
+	    bufSum == NULL ||
+	    fullBufs == NULL ||
+	    writer_tids == NULL ||
+	    more == NULL) {
+		fprintf(stderr, "malloc failed.\n");
 		exit(1);
 	}
 
@@ -125,24 +184,26 @@ main(int argc, char **argv)
 			exit(1);
 		}
 	flAborted = 0;
-	bufNum = 0;
-	totalWritten = 0;
 	flFinished = 0;
 	remainder = 0;
-	bufSum = 0;
-	bufSamples = 0;
+	for (i = 0; i < destCount; i++) {
+		totalWritten[i] = 0;
+		bufSum[i] = 0;
+		bufSamples[i] = 0;
+		fullBufs[i] = 0;
+		MYASSERT(pthread_cond_init(&more[i], NULL) == 0,
+		    "pthread_cond_init failed");
+	}
 	MYASSERT(pthread_mutex_init(&lock, NULL) == 0,
 	    "pthread_mutex_init failed");
 	MYASSERT(pthread_cond_init(&less, NULL) == 0,
-	    "pthread_cond_init failed");
-	MYASSERT(pthread_cond_init(&more, NULL) == 0,
 	    "pthread_cond_init failed");
 	MYASSERT(pthread_attr_init(&attr) == 0,
 	    "pthread_attr_init failed");
 	MYASSERT(pthread_attr_setdetachstate(&attr,
 	    PTHREAD_CREATE_DETACHED) == 0,
 	    "pthread_attr_setdetachstate failed");
-	MYASSERT(pthread_create(&tid, &attr, &reader, NULL) == 0,
+	MYASSERT(pthread_create(&reader_tid, &attr, &reader, NULL) == 0,
 	    "pthread_create failed");
 	MYASSERT(pthread_attr_destroy(&attr) == 0,
 	    "pthread_attr_destroy failed");
@@ -153,49 +214,20 @@ main(int argc, char **argv)
 	}
 	signal(SIGINT, &cleanup);
 	MYASSERT(gettimeofday(&tpstart, NULL) == 0, "gettimeofday failed");
-	while (!flAborted && !(flFinished && fullBufs == 0)) {
-		/* wait for block of data */
-		MYASSERT(pthread_mutex_lock(&lock) == 0,
-		    "pthread_mutex_lock failed");
-		while (fullBufs == 0 && !flAborted) {
-			MYASSERT(pthread_cond_wait(&more, &lock) == 0,
-			    "pthread_cond_wait failed");
-		}
-		if (flAborted)
-			break;
-		MYASSERT(pthread_mutex_unlock(&lock) == 0,
-		    "pthread_mutex_unlock failed");
-		/* write it */
-		if (flFinished && fullBufs == 1)
-			/* write the remainder, partial block */
-			writeSize = remainder;
-		else
-			writeSize = bufSize;
-		numWrite = write(outfd, buf[bufNum], writeSize);
-		if (numWrite != writeSize) {
-			if (numWrite != -1) {
-				fprintf(stderr, "Short write: "
-				    "%" PRId64 " bytes.\n",
-				    (int64_t)numWrite);
-				flAborted = 1;
-				totalWritten += numWrite;
-			} else
-				perror("Write failed");
-			break;
-		}
-		totalWritten += numWrite;
-		if (++bufNum >= numBufs)
-			bufNum = 0;
-		MYASSERT(pthread_mutex_lock(&lock) == 0,
-		    "pthread_mutex_lock failed");
-		fullBufs--;
-		bufSum += fullBufs;
-		MYASSERT(pthread_cond_signal(&less) == 0,
-		    "pthread_cond_signal failed");
-		MYASSERT(pthread_mutex_unlock(&lock) == 0,
-		    "pthread_mutex_unlock failed");
-		bufSamples++;
+
+	/* start the writer threads */
+
+	for (i = 0; i < destCount; i++) {
+		MYASSERT(pthread_create(&writer_tids[i], NULL, &writer,
+		    (void *)i) == 0,
+			"pthread_create failed");
 	}
+
+	/* wait for the writer threads to finish */
+
+	for (i = 0; i < destCount; i++)
+		pthread_join(writer_tids[i], NULL);
+
 	MYASSERT(gettimeofday(&tpend, NULL) == 0, "gettimeofday failed");
 	duration = tpend.tv_sec + tpend.tv_usec / 1000000.0 -
 	    tpstart.tv_sec - tpstart.tv_usec / 1000000.0;
@@ -204,19 +236,42 @@ main(int argc, char **argv)
 	if (flAborted)
 		fprintf(stderr, "Transfer aborted.\n");
 	if (!flQuiet) {
+		uint64_t totalWrittenSum;
+		unsigned long bufSamplesSum, bufSumSum;
+
+		totalWrittenSum = bufSamplesSum = bufSumSum = 0;
+		for (i = 0; i < destCount; i++) {
+			totalWrittenSum += totalWritten[i];
+			bufSamplesSum += bufSamples[i];
+			bufSumSum += bufSum[i];
+		}
+		
 		fprintf(stderr, "%" PRId64
 		    " bytes transferred in %.3f secs (%.3f KiB/sec)\n"
 		    "%ld partial read%s, %.3f average buffers full\n",
-		    totalWritten, duration,
+		    totalWrittenSum, duration,
 		    duration > 0 ?
-		      (float)totalWritten / duration / 1024.0 : 0.0,
+		      (float)totalWrittenSum / duration / 1024.0 : 0.0,
 		    partialReads, partialReads != 1 ? "s" : "",
-		    bufSamples > 0 ? (float)bufSum / bufSamples : 0.0);
+		    bufSamplesSum > 0 ? (float)bufSumSum / bufSamplesSum : 0.0);
 	}
 	if (flAborted)
-		pthread_cancel(tid);
+		pthread_cancel(reader_tid);
 
 	return 0;
+}
+
+int
+maxFullBufs(void)
+{
+	int fullBufsMax;
+	int i;
+	
+	fullBufsMax = fullBufs[0];
+	for (i = 1; i < destCount; i++)
+		if (fullBufs[i] > fullBufsMax)
+			fullBufsMax = fullBufs[i];
+	return fullBufsMax;
 }
 
 static void *
@@ -224,6 +279,7 @@ reader(void *dummy)
 {
 	ssize_t numRead, totalRead;
 	int bufNum;
+	int i;
 
 	bufNum = 0;
 	partialReads = 0;
@@ -231,11 +287,13 @@ reader(void *dummy)
 		totalRead = 0;
 		MYASSERT(pthread_mutex_lock(&lock) == 0,
 		    "pthread_mutex_lock failed");
-		while (fullBufs == numBufs)
+		while (maxFullBufs() == numBufs && !flAborted)
 			MYASSERT(pthread_cond_wait(&less, &lock) == 0,
 			    "pthread_cond_wait failed");
 		MYASSERT(pthread_mutex_unlock(&lock) == 0,
 		    "pthread_mutex_unlock");
+		if (flAborted)
+			break;
 		do {
 			numRead = read(STDIN_FILENO, &(buf[bufNum][totalRead]),
 					bufSize - totalRead);
@@ -250,7 +308,8 @@ reader(void *dummy)
 				default:
 					perror("Read failed");
 					flAborted = 1;
-					pthread_cond_signal(&more);
+					for (i = 0; i < destCount; i++)
+						pthread_cond_signal(&more[i]);
 					return NULL;
 				}
 			}
@@ -262,17 +321,84 @@ reader(void *dummy)
 			if (numRead != bufSize)
 				partialReads++;
 			totalRead += numRead;
-		} while (totalRead != bufSize);
+		} while (!flAborted && totalRead != bufSize);
 		if (++bufNum >= numBufs)
 			bufNum = 0;
 		MYASSERT(pthread_mutex_lock(&lock) == 0,
 		    "pthread_mutex_lock failed");
-		fullBufs++;
-		MYASSERT(pthread_cond_signal(&more) == 0,
-		    "pthread_cond_signal failed");
+		for (i = 0; i < destCount; i++) {
+			fullBufs[i]++;
+			MYASSERT(pthread_cond_signal(&more[i]) == 0,
+			    "pthread_cond_signal failed");
+		}
 		MYASSERT(pthread_mutex_unlock(&lock) == 0,
 		    "pthread_mutex_unlock");
 	}
+
+	/* tell the writers to give up if we've been aborted */
+
+	if (flAborted)
+		for (i = 0; i < destCount; i++)
+			pthread_cond_signal(&more[i]);
+	return NULL;
+}
+
+static void *
+writer(void *destNum)
+{
+	ssize_t numWrite, writeSize;
+	int bufNum = 0;
+	int tid = (int) destNum;
+
+	while (!flAborted && !(flFinished && fullBufs[tid] == 0)) {
+		/* wait for block of data */
+		MYASSERT(pthread_mutex_lock(&lock) == 0,
+		    "pthread_mutex_lock failed");
+		while (fullBufs[tid] == 0 && !flAborted) {
+			MYASSERT(pthread_cond_wait(&more[tid], &lock) == 0,
+			    "pthread_cond_wait failed");
+		}
+		MYASSERT(pthread_mutex_unlock(&lock) == 0,
+		    "pthread_mutex_unlock failed");
+		if (flAborted)
+			break;
+		/* write it */
+		if (flFinished && fullBufs[tid] == 1)
+			/* write the remainder, partial block */
+			writeSize = remainder;
+		else
+			writeSize = bufSize;
+		numWrite = write(outfds[tid], buf[bufNum], writeSize);
+		if (numWrite != writeSize) {
+			if (numWrite != -1) {
+				fprintf(stderr, "%s: Short write: "
+				    "%" PRId64 " bytes.\n",
+				    destNames[tid],
+				    (int64_t)numWrite);
+				flAborted = 1;
+				totalWritten[tid] += numWrite;
+			} else
+				perror("Write failed");
+			break;
+		}
+		totalWritten[tid] += numWrite;
+		if (++bufNum >= numBufs)
+			bufNum = 0;
+		MYASSERT(pthread_mutex_lock(&lock) == 0,
+		    "pthread_mutex_lock failed");
+		fullBufs[tid]--;
+		bufSum[tid] += fullBufs[tid];
+		MYASSERT(pthread_cond_signal(&less) == 0,
+		    "pthread_cond_signal failed");
+		MYASSERT(pthread_mutex_unlock(&lock) == 0,
+		    "pthread_mutex_unlock failed");
+		bufSamples[tid]++;
+	}
+
+	/* tell the reader to give up if we've been aborted */
+
+	if (flAborted)
+		pthread_cond_signal(&less);
 	return NULL;
 }
 
@@ -280,7 +406,7 @@ static void *
 status(void *dummy)
 {
 	while (!flAborted && !flFinished) {
-		statusLine(totalWritten / 1024, 0, "KiB", "KiB/s");
+		statusLine(totalWritten[0] / 1024, 0, "KiB", "KiB/s");
 		usleep(STATUS_UPDATE_TIME);
 	}
 	fputc('\n', stderr);
@@ -297,13 +423,14 @@ static void
 usage()
 {
 	fprintf(stderr, "mbdd version " PACKAGE_VERSION ".\n"
-	    "Copyright Paul Ripke $Date: 2008/09/17 10:17:19 $\n");
+	    "Copyright Paul Ripke $Date: 2008/09/17 10:51:24 $\n");
 	fprintf(stderr, "Multi-buffer dd\n\n");
 	fprintf(stderr, "Built to use pthreads.\n\n");
-	fprintf(stderr, "Usage: mbdd [-b bytes] [-n number] [-q]\n\n");
+	fprintf(stderr, "Usage: mbdd [-b bytes] [-n number] [-qs]\n\n");
 	fprintf(stderr, "  -b bytes    Set buffer size\n");
 	fprintf(stderr, "  -n number   Number of buffers\n");
 	fprintf(stderr, "  -q          Quiet operation\n");
+	fprintf(stderr, "  -s          Suppress write to stdout\n");
 	fprintf(stderr, "  -v          Display progress line\n\n");
 	fprintf(stderr, "Compiled defaults:\n");
 	fprintf(stderr, "    mbdd -b 64k -n 16\n\n");
